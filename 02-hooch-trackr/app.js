c2657a7ebbdd3904d6d4270ae7670a60d0f1859d
aneylon <arlen.m.neylon@gmail.com>
2018-12-09 17:25:58 -0700

    Setting up for filter searching.

--- 02-hooch-trackr/app.js
+++ 02-hooch-trackr/app.js
@@ -1,10 +1,23 @@
 console.log('trackr it')
 
+Vue.component('list-item', {
+  props: ['item'],
+  template: '<li> {{ item.name }} : {{ item.rating}} </li>'
+})
+
 var trackr = new Vue({
   el: '#trackr',
   data: {
     appName: 'Hooch Trackr',
-    aLink: '<a href="/">Link</a>',
-    appLogo: '/public/img/logo.png'
+    searchString: '',
+    entries: [
+      { id: 0, name: '4 Roses', description: '', rating: 3 },
+      { id: 1, name: 'Jim Beam', description: '', rating: 2 }
+    ]
+  },
+  methods: {
+    searchEntries: function() {
+      console.log('searching entries')
+    }
   }
 })
